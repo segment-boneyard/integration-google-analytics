32dbe179e048f86205d374dbeabd16fc29fbcb7f
Nathan Houle <nathan@nathanhoule.com>
2015-04-30 17:39:34 -0700

    Use strict mode everywhere

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var facade = require('segmentio-facade');
 var merge = require('merge-util');
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var Test = require('segmentio-integration-tester');
 var helpers = require('./helpers');
